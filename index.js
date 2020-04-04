2cdede943ee8d9b481d7eb65f863681da2fedfb5
Your Name <you@example.com>
2020-04-04 15:51:56 +0100

    make it betterer2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+var port = process.env.PORT || 3000;
 
 //app.use(express.static('public'))app.get('/', (req, res) => res.send('Hello World! and all the things!!!'))
 
